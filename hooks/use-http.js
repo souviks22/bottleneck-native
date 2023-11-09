187fbdbdb4519d69332d9bb04b90d1bbd590f9bf
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-11-09 22:59:03 +0530

    Http hook added

---
+++ hooks/use-http.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+import Constants from "expo-constants"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+const { api, tokenKey } = Constants.expoConfig.extra
+
+export const useHttp = () => {
+    const [isLoading, setIsLoading] = useState()
+    const httpRequest = async (url, method = 'get', body = null) => {
+        try {
+            setIsLoading(true)
+            const token = await AsyncStorage.getItem(tokenKey)
+            const res = await fetch(`${api}${url}`, {
+                method, body,
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${token}`
+                }
+            })
+            const { success, message, data } = await res.json()
+            setIsLoading(false)
+            if (!success) throw new Error(message)
+            return data
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return { httpRequest, isLoading }
+}
